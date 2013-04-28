41b47abd2ec45bfbaf26d4113492421e114315f1
quadtree <admin@ironalloygames.com>
2013-04-28 01:21:31 -0700

    Dialog

--- core/src/main/java/com/ironalloygames/planetfall/dialog/EnemyDoctorDialog.java
+++ core/src/main/java/com/ironalloygames/planetfall/dialog/EnemyDoctorDialog.java
@@ -24,26 +24,21 @@ public class EnemyDoctorDialog extends Dialog {
 		}
 		
 		states.put("Start", new HashMap<String, StateTransition>());
-		states.get("Start").put("P", new StateTransition("From seeing the outside of this pod, you're very suspicious, so you enter with weapon drawn.", ""));
+		if(armed)
+			states.get("Start").put("P", new StateTransition("From seeing the outside of this pod, you're very suspicious, so you enter with weapon drawn.", ""));
+		else
+			states.get("Start").put("P", new StateTransition("You enter the unknown pod.", ""));
 		states.get("Start").put("1", new StateTransition("Continue", "Start2"));
 		
 		states.put("Start2", new HashMap<String, StateTransition>());
 		states.get("Start2").put("P", new StateTransition("In the pod, sitting on the floor is a suprised young " + PFG.s.enemyDoctor.pd.getManWoman() + ", wearing the uniform of the " +
 				PFG.s.enemyEmpire.name + " space forces. " + PFG.s.enemyDoctor.pd.getHeShe() + " reaches for a fusion lancer pistol lying on the floor.", ""));
 		int x = 1;
-		states.get("Start2").put("" + x++, new StateTransition("Yell \"Freeze!\", holding your weapon out", "Freeze"));
+		if(armed)
+			states.get("Start2").put("" + x++, new StateTransition("Yell \"Freeze!\", holding your weapon out", "Freeze"));
+		else
+			states.get("Start2").put("" + x++, new StateTransition("Hold your hands out, palms first", "Unarmed"));
 		
-		if(hasSpear) states.get("Start2").put("" + x++, new StateTransition("Charge and run " + PFG.s.enemyDoctor.pd.getHimHer() + " through with your spear", "SpearAttack"));
-		if(hasJavelin) states.get("Start2").put("" + x++, new StateTransition("Throw your javelin at " + PFG.s.enemyDoctor.pd.getHimHer(), "JavelinAttack"));
-		if(hasGun) states.get("Start2").put("" + x++, new StateTransition("Shoot " + PFG.s.enemyDoctor.pd.getHimHer() + " with your pistol", "GunAttack"));
-		
-		states.put("SpearAttack", new HashMap<String, StateTransition>());
-		states.get("SpearAttack").put("P", new StateTransition("Charging an enemy armed with a fusion lancer pistol armed only with a spear turns out to be a bad idea. The last thing you see is a jet of blue-green light cutting through your chest.", ""));
-		states.get("SpearAttack").put("1", new StateTransition("Continue", "End"));
-		
-		states.put("SpearAttack", new HashMap<String, StateTransition>());
-		states.get("SpearAttack").put("P", new StateTransition("Your javelin strikes " + PFG.s.enemyDoctor.pd.getHimHer() + " in the chest right as " + PFG.s.enemyDoctor.pd.getHeShe() +
-				" is levelling " + PFG.s.enemyDoctor.pd.getHisHer() + " pistol " + PFG.s.enemyDoctor.pd.getHeShe() + " crumples over backwards.", ""));
-		states.get("SpearAttack").put("1", new StateTransition("Continue", "End"));
+		states.get("Start2").put("" + x++, new StateTransition("Duck around the corner", "Duck1"));
 	}
 }
