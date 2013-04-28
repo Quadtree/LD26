888ce8efd6d8d21634166c56d5104ec9db786867
quadtree <admin@ironalloygames.com>
2013-04-28 01:49:46 -0700

    Dialog!

--- core/src/main/java/com/ironalloygames/planetfall/dialog/EnemyDoctorDialog.java
+++ core/src/main/java/com/ironalloygames/planetfall/dialog/EnemyDoctorDialog.java
@@ -17,28 +17,39 @@ public class EnemyDoctorDialog extends Dialog {
 		}
 		
 		states.put("Start", new HashMap<String, StateTransition>());
-		states.get("Start").put("P", new StateTransition("How do you enter the pod", ""));
-		int x = 1;
 		if(armed)
-			states.get("Start").put("" + x++, new StateTransition("Enter with weapon drawn", "Armed1"));
+			states.get("Start").put("P", new StateTransition("You enter the pod, weapon drawn", ""));
+		else
+			states.get("Start").put("P", new StateTransition("You enter the pod", ""));
 		
-		states.get("Start").put("" + x++, new StateTransition("Enter unarmed", "Unarmed1"));
+		states.get("Start2").put("1", new StateTransition("Continue", "Start2"));
 		
-		states.put("Armed", new HashMap<String, StateTransition>());
-		states.get("Armed").put("P", new StateTransition("In the pod, sitting on the floor is a suprised young " + PFG.s.enemyDoctor.pd.getManWoman() + ", wearing the uniform of the " +
+		states.put("Start2", new HashMap<String, StateTransition>());
+		states.get("Start2").put("P", new StateTransition("In the pod, sitting on the floor is a suprised young " + PFG.s.enemyDoctor.pd.getManWoman() + ", wearing the uniform of the " +
 				PFG.s.enemyEmpire.name + " space forces. " + PFG.s.enemyDoctor.pd.getHeShe() + " reaches for a fusion lancer pistol lying on the floor.", ""));
-		states.get("Armed").put("1", new StateTransition("Yell \"Freeze!\", holding your weapon out", "Freeze"));
-		states.get("Armed").put("2", new StateTransition("Duck around the corner", "Duck1"));
+		states.get("Start2").put("1", new StateTransition("Duck around the corner, and retreat", "Retreat"));
+		if(armed)
+			states.get("Start2").put("1", new StateTransition("Yell \"Freeze!\", holding your weapon out", "Freeze"));
 		
-		states.put("Unarmed1", new HashMap<String, StateTransition>());
-		states.get("Unarmed1").put("P", states.get("Armed").get("P"));
-		states.get("Unarmed1").put("1", new StateTransition("Hold your hands out, palms first", "Unarmed"));
-		states.get("Unarmed1").put("2", new StateTransition("Duck around the corner", "Duck1"));
+		states.put("Freeze", new HashMap<String, StateTransition>());
+		states.get("Freeze").put("P", new StateTransition("The " + PFG.s.enemyDoctor.pd.getManWoman() + " seems to consider trying to make a break for the pistol, but decides against it.", ""));
+		states.get("Freeze").put("1", new StateTransition("Approach " + PFG.s.enemyDoctor.pd.getHimHer() + " and restrain " + PFG.s.enemyDoctor.pd.getHimHer(), "Restrain"));
 		
-		states.put("Duck1", new HashMap<String, StateTransition>());
-		states.get("Duck1").put("P", new StateTransition("You hear " + PFG.s.enemyDoctor.pd.getHimHer() + " yell, \"Why are you here? Leave!\"", ""));
-		states.get("Duck1").put("1", new StateTransition("\"No reason, i guess...\"", ""));
-		states.get("Duck1").put("2", new StateTransition("Silently leave", ""));
-		states.get("Duck1").put("3", new StateTransition("\"Listen, ", ""));
+		states.put("Restrain", new HashMap<String, StateTransition>());
+		states.get("Restrain").put("P", new StateTransition("You now have " + PFG.s.enemyDoctor.pd.getHimHer() + " completely tied up with some convienent rope that was in the pod.", ""));
+		states.get("Restrain").put("1", new StateTransition("\"Alright, lets start with your name.\"", "Name"));
+		states.get("Restrain").put("1", new StateTransition("\"Are there any more of you nearby?\"", "Allies"));
+		states.get("Restrain").put("1", new StateTransition("\"Hah, not so tough now, " + PFG.s.enemyEmpire.name + " dog!\"", "Name"));
 	}
+
+	@Override
+	public void newState(String stateName) {
+		if(stateName.equals("Retreat")){
+			PFG.s.pc.x -= 3;
+			PFG.s.pc.y -= 3;
+		}
+		super.newState(stateName);
+	}
+	
+	
 }
